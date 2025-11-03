69ee29c1da3479d1db6ecf73a65326d089d3ea5c
mojiri00 <moguricamera@gmail.com>
2025-11-03 19:22:50 +0900

    refactor(Rank.java): 출력의 switch문을 없애기 위한 리팩터링 - 각 상금별 메시지 출력을 위한 lavel 필드 추가 - getter 메소드 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -2,26 +2,30 @@ package lotto;
 
 public enum Rank {
 
-    FIRST (6, false, 2_000_000_000),
-    SECOND(5, true ,    30_000_000),
-    THIRD (5, false,     1_500_000),
-    FOURTH(4, false,        50_000),
-    FIFTH (3, false,         5_000),
-    MISS  (0, false,             0);
+    FIRST (6, false, 2_000_000_000, ""),
+    SECOND(5, true ,    30_000_000, "보너스 볼 일치"),
+    THIRD (5, false,     1_500_000, ""),
+    FOURTH(4, false,        50_000, ""),
+    FIFTH (3, false,         5_000, ""),
+    MISS  (0, false,              0, "");
 
     private final int matchCount;
     private final boolean requiresBonus;
     private final int prize;
+    private final String label;
 
-    Rank(int matchCount, boolean requiresBonus, int prize) {
+    Rank(int matchCount, boolean requiresBonus, int prize, String label) {
+
         this.matchCount = matchCount;
         this.requiresBonus = requiresBonus;
         this.prize = prize;
+        this.label = label;
     }
 
-    public int prize() {
-        return prize;
-    }
+    public int getMatchedCount() { return matchCount; }
+    public boolean getRequiresBonus() { return requiresBonus;}
+    public String getLabel() { return label; }
+    public int getPrize() {return prize;}
 
     /**
      * 일치 개수와 보너스 여부로 등수 판정
